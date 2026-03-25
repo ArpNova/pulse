c68b00d3e7121fd467a8c8b4cd84d586f7b398af
ArpNova <arpanbiswas.myworld@gmail.com>
2026-03-25 18:33:25 +0530

    feat: Refactor `formatSpeed` to `formatBytes` for both speed and volume units, and display total session data transferred.

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -8,20 +8,21 @@
 #include <thread>
 #include <optional>
 
-std::string formatSpeed(unsigned long long bytes) {
-  double speed = static_cast<double>(bytes);
+std::string formatBytes(unsigned long long bytes, bool isSpeed = true) {
+  double value = static_cast<double>(bytes);
 
-  const std::string units[] = {"B/s", "KB/s", "MB/s", "GB/s"};
+  const std::string speedUnits[] = {"B/s", "KB/s", "MB/s", "GB/s"};
+  const std::string volumeUnits[] = {"B", "KB", "MB", "GB"};
   int unitIndex = 0;
 
-  while (speed >= 1024.0 && unitIndex < 3) {
-    speed /= 1024.0;
+  while (value >= 1024.0 && unitIndex < 3) {
+    value /= 1024.0;
     unitIndex++;
   }
-  std::stringstream formattedSpeed;
-  formattedSpeed << std::fixed << std::setprecision(2) << speed << " "
-                 << units[unitIndex];
-  return formattedSpeed.str();
+  std::stringstream formattedOutput;
+  formattedOutput << std::fixed << std::setprecision(2) << value << " "
+                 << (isSpeed ? speedUnits[unitIndex] : volumeUnits[unitIndex]);
+  return formattedOutput.str();
 }
 
 std::optional<unsigned long long> getBytes(const std::string &interfaceName, const std::string &type) {
@@ -62,6 +63,9 @@ int main(int argc, char *argv[]) {
 
   std::cout << "Starting pulse... Monitoring " << interface << ".\nPress Ctrl+C to stop.\n\n";
 
+  unsigned long long totalRxSession = 0;
+  unsigned long long totalTxSession = 0;
+
   auto lastTime = std::chrono::steady_clock::now();
 
   while (true) {
@@ -85,13 +89,19 @@ int main(int argc, char *argv[]) {
     unsigned long long rxDiff = (currentRxBytes >= previousRxBytes) ? (currentRxBytes - previousRxBytes) : currentRxBytes;
     unsigned long long txDiff = (currentTxBytes >= previousTxBytes) ? (currentTxBytes - previousTxBytes) : currentTxBytes;
 
+    totalRxSession += rxDiff;
+    totalTxSession += txDiff;
+
     unsigned long long speedRxBytes = static_cast<unsigned long long>(rxDiff / elapsed.count());
     unsigned long long speedTxBytes = static_cast<unsigned long long>(txDiff / elapsed.count());
 
-    std::string formattedRx = formatSpeed(speedRxBytes);
-    std::string formattedTx = formatSpeed(speedTxBytes);
+    std::string speedRX = formatBytes(speedRxBytes, true);
+    std::string speedTX = formatBytes(speedTxBytes, true);
 
-    std::cout << "\rRx: " << formattedRx << "  |  Tx: " << formattedTx << "          " << std::flush;
+    std::string totRx = formatBytes(totalRxSession, false);
+    std::string totTx = formatBytes(totalTxSession, false);
+
+    std::cout << "\rRx: " << speedRX << " ("<<totRx << ") |  Tx: " << speedTX << " ("<<totTx << ")          " << std::flush;
 
     previousRxBytes = currentRxBytes;
     previousTxBytes = currentTxBytes;
